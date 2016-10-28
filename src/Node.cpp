8d84c8059e9940b47bb6f10d11b1ddfbc0dece5d
alexdanili <adm.minefun@yandex.ru>
2016-10-28 21:56:58 +0300

    ver 0.1

---
+++ src/Node.cpp
@@ -0,0 +1,13 @@
+#include "Node.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <boost/algorithm/string.hpp>
+#include <unistd.h>
+
+void add_tag(std::string tag){
+		
+}
+
+
---
+++ src/parser.hpp
@@ -0,0 +1,8 @@
+class parser
+{
+
+public:
+    std::vector<std::string> take_tokins(std::string input);
+    void make_tree(std::vector<std::string>);
+};
+
