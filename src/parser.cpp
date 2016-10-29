a4ef8207b18970b95d1af6e6af29533e6df6c915
alexdanili <adm.minefun@yandex.ru>
2016-10-29 15:17:20 +0300

    v 0.6

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,44 +4,45 @@
 #include <boost/algorithm/string.hpp>
 #include "parser.hpp"
 #include <unistd.h>
+namespace Parser{
+	std::vector<std::string> take_tokins(std::string input)
+	{
 
-std::vector<std::string> take_tokins(std::string input)
-{
+	    boost::trim(input);
+	    std::regex tag_regex("<(/?[^\>]+)>");
+	    std::vector<std::string> result;
+	    while(!input.empty()) {
+		boost::trim(input);
+		auto str_begin =
+		    std::sregex_iterator(input.begin(), input.end(), tag_regex);
+		auto str_end = std::sregex_iterator();
 
-    boost::trim(input);
-    std::regex tag_regex("<(/?[^\>]+)>");
-    std::vector<std::string> result;
-    while(!input.empty()) {
-        boost::trim(input);
-        auto str_begin =
-            std::sregex_iterator(input.begin(), input.end(), tag_regex);
-        auto str_end = std::sregex_iterator();
+		std::sregex_iterator i = str_begin;
+		if(i != str_end){
+		std::smatch match = *i;
+		std::string match_str = match.str();
+		if(match.position() == 0){
+		    input.erase(0,match_str.size());
+			match_str += "1";
+		    result.push_back(match_str);
+		}else{
+		    std::string tmp;
+		    int j;
+		    for(j = 0;j<match.position();j++){
+		        tmp+=input[j];
+		    };
+		    input.erase(0,j);
+			tmp += "0";
+		    result.push_back(tmp);
+			std::string t;
+			t = match_str + "1";
+		    result.push_back(t);
+		    input.erase(0,match_str.size());
+		}
+	    }
 
-        std::sregex_iterator i = str_begin;
-        if(i != str_end){
-        std::smatch match = *i;
-        std::string match_str = match.str();
-        if(match.position() == 0){
-            input.erase(0,match_str.size());
-		match_str += "1";
-            result.push_back(match_str);
-        }else{
-            std::string tmp;
-            int j;
-            for(j = 0;j<match.position();j++){
-                tmp+=input[j];
-            };
-            input.erase(0,j);
-		tmp += "0";
-            result.push_back(tmp);
-		std::string t;
-		t = match_str + "1";
-            result.push_back(t);
-            input.erase(0,match_str.size());
-        }
-    }
-
-}
- return result;
+	}
+	 return result;
+	}
 }
 
