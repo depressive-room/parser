1306795607c9c42d1bcf4ef8a067ec3ed6ebcd8c
lex1133 <adm.minefun@yandex.ru>
2016-11-05 20:58:18 +0300

    parser test 3

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,35 +5,56 @@
 #include "parser.hpp"
 namespace Parser{
     void parse(std::shared_ptr<std::string> input)
-    {
+     {
         std::shared_ptr<Node::Base> root = std::make_shared<Node::Base>();
-        std::shared_ptr<Node::Base> curr(root);
-
+        std::shared_ptr<Node::Base> curr = root;
         std::regex reg("<(/?[^\>]+)>");
         auto str_begin =
             std::sregex_iterator(input->begin(), input->end(), reg);
         auto str_end = std::sregex_iterator();
-
         std::sregex_iterator i = str_begin;
         int last_pos = 0;
         for(;!(i==str_end);i++){
-
-            curr->children = new std::vector<std::shared_ptr<Node::Base>>;
-
              std::smatch match = *i;
              std::string match_str = match.str();
-             int pos = match.position();
-             if(pos-last_pos!= 0){
-                 std::string text_str = input->substr(last_pos,pos-last_pos);
-                 std::shared_ptr<Node::Text> text = std::make_shared<Node::Text>(text_str);
-                 std::shared_ptr<Node::Base> base = std::make_shared<Node::Base>(text);
-                    curr->children->push_back(base);
+             if(curr->children == NULL){
+              curr->children = new std::vector<std::shared_ptr<Node::Base>>;
              }
-             std::shared_ptr<Node::Tag> tag = std::make_shared<Node::Tag>(match_str);
+                 int pos = match.position();
+                 if(pos-last_pos!= 0){
+                     std::string text_str = input->substr(last_pos,pos-last_pos);
+                     std::shared_ptr<Node::Text> text = std::make_shared<Node::Text>(text_str);
+                     std::shared_ptr<Node::Base> base = std::make_shared<Node::Base>(text);
+                     base->parrent = curr;
+                     base->children = new std::vector<std::shared_ptr<Node::Base>>;
+                     int size = curr->children->size();
+                     curr = curr->children->at(size);
+                     //std::cout<<text_str<<std::endl;
+                 }
+             std::string tag_str = match[1].str();
+             std::string tmp;
+             std::shared_ptr<Node::Tag> tag = std::make_shared<Node::Tag>(tag_str);
              std::shared_ptr<Node::Base> base = std::make_shared<Node::Base>(tag);
-             curr->children->push_back(base);
+             base->parrent = curr;
+             base->children = new std::vector<std::shared_ptr<Node::Base>>;
+             if(tag_str.at(0) != '/'){
+                 curr->children->push_back(base);
+                 size_t size = curr->children->size();
+                 curr = curr->children->at(size-1);
 
-             curr = curr->children->at(0);
+             }else{
+                 tmp = tag_str.substr(1,tag_str.length()-1);
+                 do{                        
+                     if(curr->parrent->tag->name == tmp){
+                         curr->parrent->children->push_back(base);
+                         int size = curr->children->size();
+                         curr = curr->children->at(size);
+                         break;
+                     }
+                     curr = curr->parrent;
+                 }while(curr->parrent != nullptr);
+             }
+             //std::cout<<tag<<std::endl;
              last_pos = match.position()+match_str.size();
 
         }
